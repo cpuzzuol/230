dcaa83eae4cf21c0daad99ba62ac21f702c853bc
emulibrary <emulibrary@gmail.com>
2019-02-26 00:38:27 -0500

    initial commit

---
+++ assets/js/chapter1.js
@@ -0,0 +1,29 @@
+var chapterData = [
+  {
+    number: 1,
+    type: 'multiple',
+    question: 'Which of these does not rhyme?',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 2,
+    type: 'multiple',
+    question: 'Which of these does rhyme with "bob"?',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  }
+]
+
---
+++ assets/js/chapter2.js
@@ -0,0 +1,16 @@
+var chapterData = [
+  {
+    number: 1,
+    type: 'multiple',
+    question: 'How many pieces do you wish?',
+    choices: [
+      '7',
+      '20',
+      'Infinity',
+      '0'
+    ],
+    answerIndex: 2, // choices[2]
+    explanation: 'All the bubblegum!'
+  }
+]
+
