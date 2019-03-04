3dde4f53ce128ca44acaaab0a44aa9d60a7f901c
emulibrary <emulibrary@gmail.com>
2019-03-04 01:00:41 -0500

    most of the interface is done

--- assets/js/chapter1.js
+++ assets/js/chapter1.js
@@ -2,7 +2,33 @@ var chapterData = [
   {
     number: 1,
     type: 'multiple',
-    question: 'Which of these does not rhyme?',
+    question: 'Q1 Which of these does not rhyme?',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: 'ch1/1-1.png'
+  },
+  {
+    number: 2,
+    type: 'multiple',
+    question: 'Q2 Which of these does rhyme with "bob"?',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 3,
+    type: 'multiple',
+    question: 'Q3',
     choices: [
       '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
       'bargle',
@@ -13,9 +39,113 @@ var chapterData = [
     explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
   },
   {
-    number: 2,
+    number: 4,
     type: 'multiple',
-    question: 'Which of these does rhyme with "bob"?',
+    question: 'Q4',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 5,
+    type: 'multiple',
+    question: 'Q5',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 6,
+    type: 'multiple',
+    question: 'Q6',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 7,
+    type: 'multiple',
+    question: 'Q7',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 8,
+    type: 'multiple',
+    question: 'Q8',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 9,
+    type: 'multiple',
+    question: 'Q9',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 10,
+    type: 'multiple',
+    question: 'Q10',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 11,
+    type: 'multiple',
+    question: 'Q11',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 12,
+    type: 'multiple',
+    question: 'Q12',
     choices: [
       'slob',
       'tub',
--- assets/js/chapter2.js
+++ assets/js/chapter2.js
@@ -2,15 +2,158 @@ var chapterData = [
   {
     number: 1,
     type: 'multiple',
-    question: 'How many pieces do you wish?',
+    question: 'Q1 Which of these does not rhyme?',
     choices: [
-      '7',
-      '20',
-      'Infinity',
-      '0'
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
     ],
-    answerIndex: 2, // choices[2]
-    explanation: 'All the bubblegum!'
+    answerIndex: 3, // choices[3]
+    explanation: 'ch1/1-1.png'
+  },
+  {
+    number: 2,
+    type: 'multiple',
+    question: 'Q2 Which of these does rhyme with "bob"?',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 3,
+    type: 'multiple',
+    question: 'Q3',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 4,
+    type: 'multiple',
+    question: 'Q4',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 5,
+    type: 'multiple',
+    question: 'Q5',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 6,
+    type: 'multiple',
+    question: 'Q6',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 7,
+    type: 'multiple',
+    question: 'Q7',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 8,
+    type: 'multiple',
+    question: 'Q8',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 9,
+    type: 'multiple',
+    question: 'Q9',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 10,
+    type: 'multiple',
+    question: 'Q10',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
+  },
+  {
+    number: 11,
+    type: 'multiple',
+    question: 'Q11',
+    choices: [
+      '$x(t)=5\cos(t/2+3)$ is a pure cosine $5\cos(t/2)$',
+      'bargle',
+      'fargle',
+      'jimbob'
+    ],
+    answerIndex: 3, // choices[3]
+    explanation: '$5\cos((t-(-6))/2)=5\cos(t/2+3)=x(t)$'
+  },
+  {
+    number: 12,
+    type: 'multiple',
+    question: 'Q12',
+    choices: [
+      'slob',
+      'tub',
+      'nub',
+      'rub'
+    ],
+    answerIndex: 0,
+    explanation: 'Just think about it...'
   }
 ]
 
